1b21d5a2cb0b7b4605b03f8ed908a3f5d152cade
Rana Alaa <ranaalaa18112004@gmail.com>
2024-09-17 10:21:22 +0300

    v1.0.0

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <filesystem>
+#include <string>
+
+
+using namespace std;
+namespace fs = std::filesystem;
+
+void FileOrganize(const string &path) {
+    fs::path dir(path);
+
+
+    fs::path images = dir / "images";
+    fs::path doc = dir / "doc";
+    fs::path vid = dir / "vid";
+    fs::path other = dir / "other";
+
+    fs::create_directory(images);
+    fs::create_directory(doc);
+    fs::create_directory(vid);
+    fs::create_directory(other);
+
+    for (const auto &file : fs::directory_iterator(dir)) {
+        if (fs::is_regular_file(file)) {  // Ensure that it's a file
+            string ext = file.path().extension().string();
+
+            fs::path destination;
+            if (ext == ".jpg" || ext == ".png" || ext == ".gif") {
+                destination = images / file.path().filename();
+            } else if (ext == ".pdf" || ext == ".docx" || ext == ".doc") {
+                destination = doc / file.path().filename();
+            } else if (ext == ".mp4" || ext == ".avi" || ext == ".mkv") {
+                destination = vid / file.path().filename();
+            } else {
+                destination = other / file.path().filename();
+            }
+
+
+        }
+    }
+
+}
+
+int main() {
+    string path;
+    cout << "Enter the path: ";
+    getline(cin, path);
+    FileOrganize(path);
+    return 0;
+}
